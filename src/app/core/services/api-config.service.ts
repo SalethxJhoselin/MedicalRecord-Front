d5a6cc65c7517bbc961e462574d412acf748dc5b
Jalter-ego <jhademels123@gmail.com>
2024-11-01 17:19:13 -0400

    url railway :v

--- src/app/core/services/api-config.service.ts
+++ src/app/core/services/api-config.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiConfigService {
-  readonly railwayUrl: string = 'http://localhost:8080/';
+  readonly railwayUrl: string = 'https://medicalrecord-backend-production.up.railway.app/';
   constructor() { }
 }
