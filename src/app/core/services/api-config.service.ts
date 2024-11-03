24441abfa8fbb8dd1b9c18ceb162b4a3cba96e8e
Jalter-ego <jhademels123@gmail.com>
2024-11-03 09:31:50 -0400

    render url, funciona un poco mejor que railway  al parecer

--- src/app/core/services/api-config.service.ts
+++ src/app/core/services/api-config.service.ts
@@ -5,5 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class ApiConfigService {
   readonly railwayUrl: string = 'https://medicalrecord-backend-production.up.railway.app/';
+  readonly renderUrl: string = 'https://medicalrecord-backend.onrender.com/';
   constructor() { }
 }
